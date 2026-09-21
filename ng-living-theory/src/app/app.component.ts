0e5351a4db1caaca42c13399b86a3b54a98a6234
Jabi <michal.jabi@gmail.com>
2026-09-21 11:25:48 +0200

    feat(01): task solved.

--- ng-living-theory/src/app/app.component.ts
+++ ng-living-theory/src/app/app.component.ts
@@ -9,7 +9,7 @@ import { MenuComponent } from './common/menu.component';
     <main class="container mb-5">
       <header class="hero is-info">
         <div class="hero-body">
-          <h1 class="title">Angular Live Theory</h1>
+          <h1 class="title">Angular Live Theory ?</h1>
           <h4 class="subtitle">sprawdź jak działa {{ title }}!...</h4>
         </div>
       </header>
