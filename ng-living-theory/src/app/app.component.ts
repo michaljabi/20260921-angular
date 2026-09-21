04550fd575a9868e331c90a01ce2e4f2107e7105
Jabi <michal.jabi@gmail.com>
2026-09-21 11:59:46 +0200

    task(02): ng CLI example (--dry-run) + data binding {{ }} explained (initial).

--- ng-living-theory/src/app/app.component.ts
+++ ng-living-theory/src/app/app.component.ts
@@ -9,7 +9,7 @@ import { MenuComponent } from './common/menu.component';
     <main class="container mb-5">
       <header class="hero is-info">
         <div class="hero-body">
-          <h1 class="title">Angular Live Theory ?</h1>
+          <h1 class="title">Angular Live Theory {{ two }}</h1>
           <h4 class="subtitle">sprawdź jak działa {{ title }}!...</h4>
         </div>
       </header>
@@ -24,4 +24,6 @@ import { MenuComponent } from './common/menu.component';
 })
 export class AppComponent {
   protected readonly title = 'ng-living-theory';
+
+  protected two = 2;
 }
