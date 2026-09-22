7f4a951d27c9c50d97b3fdaabf6a06b2b72f3eae
Jabi <michal.jabi@gmail.com>
2026-09-22 14:03:13 +0200

    explain(15): npm run build + dist catalog. - production version

--- auction-portal/src/main.ts
+++ auction-portal/src/main.ts
@@ -5,7 +5,9 @@ import { App } from './app/app';
 import anyWillWork, { MY_CONSTANT, MyExample, tuneTheRadio } from '../labolatory/typescript-exports'
 // import anyWillWork from '../labolatory/typescript-exports'
 
-console.log(anyWillWork);
+
+// Drobna zmiana obrazująca zmianę checksumy w pliku w dist (main-***.js)
+console.log(anyWillWork + "?");
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
