3c1461fbce119a5369491ac1ed31623e920cfba5
Jabi <michal.jabi@gmail.com>
2026-09-22 15:56:32 +0200

    task(19): really call AJAX with httpResource

--- auction-portal/src/app/auctions/auctions-page.component.ts
+++ auction-portal/src/app/auctions/auctions-page.component.ts
@@ -1,24 +1,31 @@
 import { AsyncPipe, JsonPipe } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { AuctionItem } from './auction-item';
+import { httpResource } from '@angular/common/http';
+import { AlertComponent } from '../shared/alert.component';
 
 @Component({
-  imports: [JsonPipe, AsyncPipe],
+  imports: [JsonPipe, AsyncPipe, AlertComponent],
   selector: 'app-auctions-page',
   styles: ``,
   template: `
     <section>
       <h2>Lista naszych aukcji</h2>
-
-      <div class="row">
-        @for (auciton of auctions(); track auciton.id) {
-          <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-            {{ auciton | json }}
-          </div>
-        }
-      </div>
-
-      <div>
+      @if (auctions.hasValue()) {
+        <div class="row">
+          @for (auciton of auctions.value(); track auciton.id) {
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+              {{ auciton | json }}
+            </div>
+          }
+        </div>
+      } @else if (auctions.error()) {
+        <app-alert text="Nie udało się załadować aukcji" type="alert-danger" />
+      } @else if (auctions.isLoading()) {
+        <app-alert text="Ładujemy aukcje" />
+      }
+      <hr class="my-5" />
+      <div >
         {{ singleAuction | json }}
       </div>
       <div>
@@ -29,28 +36,30 @@ import { AuctionItem } from './auction-item';
   `,
 })
 export class AuctionsPageComponent {
-  auctions = signal<AuctionItem[]>([
-    {
-      id: '2',
-      title: 'Mac ',
-      imgUrl: 'https://picsum.photos/id/48/600/600',
-      description: 'Używany - ale sprawny',
-      price: 4000,
-    },
-    {
-      id: '3',
-      title: 'Smartfon33',
-      imgUrl: 'https://picsum.photos/id/160/600/600',
-      price: 800,
-    },
-    {
-      id: '4',
-      title: 'Samochoódzzz',
-      imgUrl: 'https://picsum.photos/id/183/600/600',
-      description: 'Zabytkowy ogórek',
-      price: 1200,
-    },
-  ]);
+  auctions = httpResource<AuctionItem[]>(() => 'http://localhost:3000/auctions');
+
+  // auctions = signal<AuctionItem[]>([
+  //   {
+  //     id: '2',
+  //     title: 'Mac ',
+  //     imgUrl: 'https://picsum.photos/id/48/600/600',
+  //     description: 'Używany - ale sprawny',
+  //     price: 4000,
+  //   },
+  //   {
+  //     id: '3',
+  //     title: 'Smartfon33',
+  //     imgUrl: 'https://picsum.photos/id/160/600/600',
+  //     price: 800,
+  //   },
+  //   {
+  //     id: '4',
+  //     title: 'Samochoódzzz',
+  //     imgUrl: 'https://picsum.photos/id/183/600/600',
+  //     description: 'Zabytkowy ogórek',
+  //     price: 1200,
+  //   },
+  // ]);
 
   singleAuction: AuctionItem = {
     id: '1',
--- auction-portal/src/app/shared/alert.component.ts
+++ auction-portal/src/app/shared/alert.component.ts
@@ -6,25 +6,26 @@ import { Component, input } from '@angular/core';
   styles: ``,
   template: `
     <div class="alert" [class]="type()" role="alert">
-      {{ text() }} {{ myLuckyNumber() }} :)
+      {{ text() }}
+      <!-- {{ myLuckyNumber() }} :) -->
     </div>
   `,
 })
 export class AlertComponent {
-   text = input('Hello?!');
-   type = input('alert-info')
+  text = input('Hello?!');
+  type = input('alert-info');
 
-   myLuckyNumber = input(3);
+  myLuckyNumber = input(3);
 
-   constructor() {
-      console.log('myLuckyNumber ma wartość', this.myLuckyNumber())
-   }
+  constructor() {
+    console.log('myLuckyNumber ma wartość', this.myLuckyNumber());
+  }
 }
 
 // Angular pod spodem robi na widoku tak:
 // new AlertComponent() //#1
 // new AlertComponent() //#2
-// new AlertComponent() //#3 
+// new AlertComponent() //#3
 
 // const myInstance = new AlertComponent();
 // myInstance.text = ''
